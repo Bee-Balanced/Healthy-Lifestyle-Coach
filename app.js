93447757a2576834252834723a65616ad3c37125
“Gannon <gtr37@nau.edu>
2024-11-25 15:16:31 -0700

    Add Welcome Page

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
-const express = require("express");
-const app = express();
+// import
+import express from "express";
 
-// Define a basic route
+// create express app and define port
+const app = express();
+const PORT = 3000;
+
+// set up middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+// define route for homepage
 app.get("/", (req, res) => {
-  res.send("Hello, Express!");
+  // render the welcome.ejs template
+  res.render("welcome.ejs");
 });
 
 // Start the server
-const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
